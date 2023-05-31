ac14a0b8462c5d0f6cc745d2397ab17d02ebdf36
Jack Kolokasis <kolokasis@ics.forth.gr>
2023-05-31 12:30:56 +0300

    Add plot config file

---
+++ paper/raw/scripts/config.py
@@ -0,0 +1,117 @@
+
+###################################################
+#
+# file: config.py
+#
+# @Author:   Iacovos G. Kolokasis
+# @Version:  07-03-2021
+# @email:    kolokasis@ics.forth.gr
+#
+# Configuration file about formating plots for papers
+#
+###################################################
+
+fullfigsize = (13, 2)                      # Full figure size
+seventhfigsize = (5, 3)                       # Half figure size
+halffigsize = (7, 3)                       # Half figure size
+thirdfigsize = (5, 3)                      # One third figure size
+quartfigsize = (3.25, 3)                   # One fourth figure size
+qhquartfigsize = (1.8, 3)                   # One fourth figure size
+qquartfigsize = (4, 3)                   # One fourth figure size
+fontsize = 12                              # Font size
+linewidth = 3                              # Graphs line width
+linewidth_2 = 0.01                         # Graphs line width
+edgewidth = 0.5                            # Edge width
+edgecolor = 'k'                            # Edgecolor = Black
+hatchcolor = 'w'                            # Edgecolor = Black
+markersize = 12                            # Marker size
+
+# Color Blind pallete
+color = ['#3399ff',                # Blue
+         '#ff9933',                # Orange
+         '#00b33c',                # Green
+         '#cc0000',                # Red
+         '#9966ff',                # Purple
+         '#8b7cf6',                # Light Purple
+         '#ffc20a',                # Yellow
+         '#7cf684',                # Light Green
+         '#b27866',                # Brown
+         '#66b2b1',                # Green + Blue
+         '#d8a9d9',                # Pink
+         '#e6cae2',                # Light Pink
+         '#9ebbd9',                # Light Blue
+         '#cfd8e3',                # Light Grey + Blue
+         '#000000',                # Darg gray
+         '#b2b2b2']
+
+# Tints of black
+monochrom = ['#000000',  # Black
+             '#444444',
+             '#b4b4b4',
+             '#959595',
+             '#3a3a3a',
+             '#bbbbbb',
+             '#888888',
+             '#707070',
+             '#cccccc',
+             '#e5e5e5',
+             '#ffffff',
+             '#9ab4b6'
+             ]                  # White
+
+# Tints of black
+black_mono = ['#000000',
+             '#333333',
+             '#666666',
+             '#999999',
+             '#cccccc']
+
+blue_mono = ['#000000',
+             '#525E5F',
+             '#7F8C8D',
+             '#BDC3C7',
+             '#9ab4b6',
+             ]
+
+blue_mono2 = ['#000000',
+              '#7F8C8D',
+              '#ffffff',
+              '#525E5F',
+              '#BDC3C7',
+              ]
+
+blue = ['#2F4F4F',                         # Compalt blue
+        '#C1CDCD',                          # Blue gray
+        '#ADD8E6',
+        '#6F8FAF',
+        '#A7C7E7',
+        '#7F8C8D'
+        ]
+
+green = ['#2F4F4F',                         # Compalt blue
+         '#B4CDCD',                          # Blue gray
+         '#668B8B',
+         '#E0EEEE',
+         '#5F9F9F',
+         ]
+
+B_color_cycle = ['#3399ff',                # Blue
+                 '#ff9933',                # Orange
+                 '#00b33c',                # Green
+                 '#cc0000',                # Red
+                 '#9966ff',                # Purple
+                 '#b07ce8',                # Violet
+                 '#ff509e',                # Magenda
+                 '#ffb000',                # Gold
+                 '#c0bfc0']                # Gray
+
+line_style = ['-', '--', '-.', ':', (0, (3, 1, 1, 1))]
+marker = ['s', '^', 'o', 'D', 'X', '*', 'v', 'P']
+c_marker = ['s-', '^', 'o-', 'D', 'X', '*', 'v', 'P']
+d_patterns = ('', '////', 'xxxx', '\\\\\\', '+++',
+              '-----', '/////', '.', 'O', '*', 'ooo')
+patterns = ('', '/////', 'xxxx', '\\\\\\', '+++',
+            '-----', '/////', '.', 'O', '*', 'ooo')
+
+
+# https://matplotlib.org/stable/gallery/lines_bars_and_markers/linestyles.html
